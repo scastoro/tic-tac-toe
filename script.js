8436442407176d680382e4e8c1f44a7a74e31f72
Sal Castoro <salcastoro@gmail.com>
2022-01-11 15:51:38 -0500

    Add start/restart button and logic. Add form and form logic. Add event listener toggle. Add display element function

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const gameBoard = (function() {
     return board;
   }
   // Build logic to determine win or tie
-  // Can manually check for each win conditon
+  // Can manually check for each win conditions
   // Such as if board[0][0] === x && board[0][1] === x && board[0][3] === x  
   // Then win condition is met
   // Maybe use switch statement if going down this path
@@ -44,7 +44,7 @@ const gameBoard = (function() {
         return true;
       case (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X'):
         return true;
-      case (board[2][0] === 'X' && board[1][1] === 'X' && board[2][0] === 'X'):
+      case (board[2][0] === 'X' && board[1][1] === 'X' && board[0][2] === 'X'):
         return true;
       default:
         return false
@@ -66,7 +66,7 @@ const gameBoard = (function() {
         return true;
       case (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O'):
         return true;
-      case (board[2][0] === 'O' && board[1][1] === 'O' && board[2][0] === 'O'):
+      case (board[2][0] === 'O' && board[1][1] === 'O' && board[0][2] === 'O'):
         return true;
       default:
         return false
@@ -85,12 +85,22 @@ const gameBoard = (function() {
     return true;
   }
 
+  // Reset gameBoard
+  function resetGameBoard() {
+    for (let i = 0; i < board.length; i++) {
+      for(let j = 0; j < board[i].length; j++){
+        board[i][j] = '';
+      }
+    }
+  }
+
   return {
     updateBoard: updateBoard,
     getBoardState: getBoardState,
     checkXWin: checkXWin,
     checkOWin: checkOWin,
-    checkTie: checkTie
+    checkTie: checkTie,
+    resetGameBoard: resetGameBoard
 
   };
 })();
@@ -105,32 +115,74 @@ const gameBoard = (function() {
   // How to call updateBoard with proper letter argument
 // Make sure functions can accept both event listener input and strings so the computer can interact with the board
 const displayController = (function(){
+  let player1Name;
+  let player2Name;
+  let isAttached;
   
   // Select the DOM elements and store them in variables 
-  let displayContainer = document.querySelector('.display-container');
-  let gameBoardContainer = document.querySelector('.gameboard-container');
+  const displayContainer = document.querySelector('.display-container');
+  const gameBoardContainer = document.querySelector('.gameboard-container');
+  const startButton = document.getElementById('start-btn');
+  const form = document.querySelector('.form-container');
+  const formButton = document.getElementById('form-btn');
 
-  document.addEventListener('click', cellHandler);
-  // Render function
-  // Cell divs have data attributes describing index in array
-  // Not sure if creating an id attribute would be useful for CSS selectors
-  // Need to call render function every time board state is updated
-  // Use nested loops
-    // Outer loop creates row div
-    // Row div will need class row
-    // Inner loop will append cell divs to the row div
-    // Cell div will need class cell
+  startButton.addEventListener('click', startBtnHandler);
+  formButton.addEventListener('click', formHandler)
+
+  // Init() to start game when start game button is pressed
+  // Needs to activate the cell event listener and display first player's name in display container
+  // Need to create functions to add callbacks to event listeners and remove them
+  function startBtnHandler() {
+    if(startButton.innerText === 'Start Game'){
+      startButton.innerText = 'Reset Game';
+      form.style.display = 'block';
+      toggleClickEvent();
+    } else{
+      // startButton.innerText = 'Start Game'
+      gameBoard.resetGameBoard();
+      gameFlow.gameReset();
+      render();
+      toggleClickEvent();
+    }
+  }
+
+  function formHandler(event) {
+    event.preventDefault();
+    player1Name = form.firstElementChild.elements[0].value;
+    player2Name = form.firstElementChild.elements[1].value;
+    console.log(player1Name, player2Name);
+    form.style.display = 'none';
+    gameBoardContainer.style.display = 'flex';
+    addDisplayElement(`${player1Name}'s turn.`)
+    render();
+  }
+  // Toggle click event listener
+  function toggleClickEvent(){
+    
+    if(!isAttached){
+      document.addEventListener('click', cellHandler);
+      isAttached = true;
+      console.log(isAttached);
+    }else {
+      document.removeEventListener('click', cellHandler);
+      isAttached = false;
+      console.log(isAttached);
+    }
+  }
+
+  // Render gameBoard
   function render() {
-
+    // Clear board if present
     while (gameBoardContainer.firstChild) {
       gameBoardContainer.removeChild(gameBoardContainer.firstChild);
     }
 
     let board = gameBoard.getBoardState();
+    // Iterate through first level of board array, creating rows
     board.forEach((row, yIndex) => {
       const rowDiv = document.createElement('div');
       rowDiv.classList.toggle('row');
-
+      // Iterate through each cell, creating and populating divs
       row.forEach((cell, xIndex) => {
         const cellDiv = document.createElement('div');
         cellDiv.classList.toggle('cell');
@@ -138,7 +190,6 @@ const displayController = (function(){
         cellDiv.setAttribute('data-xIndex', xIndex);
         cellDiv.innerText = cell;
 
-        // console.log(cellDiv);
         rowDiv.appendChild(cellDiv)
       });
 
@@ -149,26 +200,50 @@ const displayController = (function(){
   function cellHandler(obj){
     if (obj.type === 'click' && obj.target.classList.contains('cell')){
       // Run game function
-      console.log(obj);
       gameFlow.runTwoPlayerGame(obj);
     }
   }
+  // Add header to display container
+  function addDisplayElement(text) {
+    while (displayContainer.firstChild) {
+      displayContainer.removeChild(displayContainer.firstChild);
+    }
 
+    const header = document.createElement('h1');
+    header.classList.add('display-header');
+    header.appendChild(document.createTextNode(text));
 
-  render();
+    displayContainer.appendChild(header);
+  }
+  // Function to handle form input for player names
+  // Can create pop up modal 
+  // Need to pass name information to gameFlow module
+
+  function getPlayer1Name(){
+    return player1Name;
+  }
+
+  function getPlayer2Name(){
+    return player2Name;
+  }
 
   return {
-    render: render
+    render: render,
+    addDisplayElement: addDisplayElement,
+    getPlayer1Name: getPlayer1Name,
+    getPlayer2Name: getPlayer2Name,
+    toggleClickEvent: toggleClickEvent
   }
   
 })();
 
 // Player factory
-const Player = letter => {
+const Player = (letter, name) => {
 
+  const getName = () => name();
   const getLetter = () => letter;
 
-  return {getLetter}
+  return {getLetter, getName}
 };
 
 // Maybe make another module to control the flow of the game itself
@@ -182,28 +257,53 @@ const Player = letter => {
 const gameFlow = (function(){
   let currentPlayer;
 
-  const player1 = Player('X');
-  const player2 = Player('O');
+  const player1 = Player('X', displayController.getPlayer1Name);
+  const player2 = Player('O', displayController.getPlayer2Name);
 
   function runTwoPlayerGame(obj){
     if(!currentPlayer) {
       currentPlayer = player1;
+      displayController.addDisplayElement(`${player2.getName()}'s turn!`);
       gameBoard.updateBoard(player1.getLetter(), obj.target.getAttribute('data-yindex'), obj.target.getAttribute('data-xindex'));
     } else if (currentPlayer === player1) {
       currentPlayer = player2;
+      displayController.addDisplayElement(`${player1.getName()}'s turn!`);
       gameBoard.updateBoard(player2.getLetter(), obj.target.getAttribute('data-yindex'), obj.target.getAttribute('data-xindex'));
+      if(gameBoard.checkOWin()){
+        displayController.addDisplayElement(`${player2.getName()} Wins!`);
+        displayController.toggleClickEvent();
+        displayController.render();
+        return
+      }
     } else if (currentPlayer === player2){
       currentPlayer = player1;
+      displayController.addDisplayElement(`${player2.getName()}'s turn!`);
       gameBoard.updateBoard(player1.getLetter(), obj.target.getAttribute('data-yindex'), obj.target.getAttribute('data-xindex'));
+      if(gameBoard.checkXWin()){
+        displayController.addDisplayElement(`${player1.getName()} Wins!`);
+        displayController.toggleClickEvent();
+        displayController.render();
+        return
+      }
     }
 
     displayController.render();
-    console.log(gameBoard.getBoardState());
+    if (gameBoard.checkTie()) {
+      displayController.addDisplayElement('Tie! No one wins!');
+      displayController.toggleClickEvent();
+    }
+    // console.log(gameBoard.getBoardState());
 
+  }
+
+  function gameReset(){
+    currentPlayer = player2;
+    displayController.addDisplayElement(`${player1.getName()}'s turn!`);    
   }
 
   return {
     runTwoPlayerGame: runTwoPlayerGame,
+    gameReset: gameReset,
     player1: player1,
     player2: player2
   }
